bb7f1e64a874905b8a5752f155ef051910c5adf9
Kiyoshi Amano <kiyoshi@Kiyoshis-MacBook-Pro.local>
2020-09-21 19:02:07 +0900

    each テスト

--- routes/hello.js
+++ routes/hello.js
@@ -5,6 +5,26 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('mydb.sqlite3');
 
 router.get('/', (req, res, next)=>{
+    db.serialize(()=>{
+        var rows = "";
+        db.each("select * from mydata", (err, row)=>{
+            if(!err){
+                rows += "<tr><th>" + row.id + "</th><td>" + row.Name + "</td></tr>"
+            }
+        }, (err, count)=>{
+            if(!err){
+                var data = {
+                    title : 'Hello !',
+                    content : rows
+                };
+                res.render('hello', data);
+            }
+        })
+    })
+})
+
+/*
+router.get('/', (req, res, next)=>{
 
     db.serialize(() => {
         db.all('select * from mydata',(err, rows) => {
@@ -17,19 +37,7 @@ router.get('/', (req, res, next)=>{
             }
         });
     });
-});
 
-/*
-router.post('/post', (req, res, next)=>{
-    var msg = req.body['message'];
-    req.session.message = msg;
-
-    var data = {
-        title : 'Hello!',
-        content : 'ゆーは、「' + msg + '」と送信したでよ'
-    }
-
-    res.render('hello', data);
 });
 */
 
